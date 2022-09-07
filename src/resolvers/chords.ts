55e486c84b31dcb9f146f8291352762518f9ad1c
saddagada1 <saddagada1@gmail.com>
2022-09-06 21:29:59 -0400

    touchup-chords-resolver

--- src/resolvers/chords.ts
+++ src/resolvers/chords.ts
@@ -1,14 +1,11 @@
-import {
-  Resolver,
-  Field,
-  ObjectType,
-  Query,
-} from "type-graphql";
-import fs from 'fs';
+import { Resolver, Field, ObjectType, Query } from "type-graphql";
+import fs from "fs";
 import path from "path";
 
 @ObjectType()
 class ChordsResponse {
+  @Field()
+  _id: string;
   @Field()
   data: string;
 }
@@ -17,9 +14,13 @@ class ChordsResponse {
 export class ChordsResolver {
   @Query(() => ChordsResponse)
   async chords(): Promise<ChordsResponse> {
-    const data = fs.readFileSync(path.resolve(__dirname, "../data/chords.json"), "utf-8",)
+    const data = fs.readFileSync(
+      path.resolve(__dirname, "../data/chords.json"),
+      "utf-8"
+    );
     return {
-      data
+      _id: "chords", 
+      data,
     };
   }
 }
